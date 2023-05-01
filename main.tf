66572549cb42aab38547c45a482458384e2f9f97
Ricardo Mourão <ricardomrf@al.insper.edu.br>
2023-05-01 20:05:42 -0300

    .tf hospeda site estatico

---
+++ main.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "site_bucket" {
+  bucket = "check-request-2"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket = aws_s3_bucket.site_bucket.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket = aws_s3_bucket.site_bucket.id
+  key    = "error.html"
+  source = "error.html"
+  content_type = "text/html"
+}
+
+
+resource "aws_s3_bucket_website_configuration" "site_bucket" {
+  bucket = aws_s3_bucket.site_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "meu_bucket" {
+  bucket = aws_s3_bucket.site_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.site_bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  version = "2012-10-17"
+  
+  statement {
+    actions = ["s3:GetObject"]
+    effect  = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = ["arn:aws:s3:::check-request-2/*"]
+  }
+}
